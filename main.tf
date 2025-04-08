0df9e4b4b0ba78370c77e9c1672baad0e4188a2f
cleverson-ingenimax <cleverson@ingenimax.ai>
2025-04-08 20:32:21 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region  = "us-west-2"
-    bucket  = "starops-terraform-s3-state-bucket"
+    bucket  = "starops-terraform-s3-state"
     key     = "aws-s3/terraform.tfstate"
     encrypt = true
   }
